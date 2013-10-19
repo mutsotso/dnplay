5e9b2b8af031a8686a6acec2e95fa039a8a58937
Mutsotso <mutsots@gmail.com>
2013-10-19 18:25:39 +0300

    initial commit with express and jade

--- src/app-server/server.js
+++ src/app-server/server.js
@@ -1,7 +1,21 @@
 require([
-	// Require in modules here
-], function(/* Provide Return Variables Here */){
+	"dojo/node!express",
+    "dojo/node!jade"
+], function(express, jade){
+    var app = express(), listenPort = 3000;
 
-	// Write server code here.
+    app.configure(function(){
+        app.locals.debug = true;
+        app.locals.pretty = true;
+        app.set('view engine', 'jade');
+        app.set('views', 'views');
+        app.use(express.logger());
+    });
 
+    app.get('/', function(request, response){
+        response.render('index');
+    });
+
+    app.listen(listenPort);
+    console.log('Listening on port 3000');
 });
